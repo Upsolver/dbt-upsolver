95f52ed1ace433eab60e9968b006d5e8bda4c171
Tanya Shemet <tanyshak@gmail.com>
2023-05-11 11:36:15 +0200

    added delete_condition example

---
+++ examples/upsert_records_new/models/dell_test2.sql
@@ -0,0 +1,15 @@
+{{ config(  materialized='incremental',
+            incremental_strategy='merge',
+            map_columns_by_name=True,
+            sync=True,
+            options={'START_FROM': 'NOW',
+              'ADD_MISSING_COLUMNS': True,
+              'RUN_INTERVAL': '1 MINUTE'},
+            primary_key=[{'field':'orderid', 'type':'string'}],
+            delete_condition='nettotal > 1000' )
+}}
+
+SELECT
+ *
+FROM {{ source('upsert_records_new', 'orders_raw_data_for_upsert_2') }}
+WHERE $event_time BETWEEN run_start_time() AND run_end_time()
