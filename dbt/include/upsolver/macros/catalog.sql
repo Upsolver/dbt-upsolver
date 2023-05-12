9fc5e033a9b669ba2d301d35e7f9f4494c18bfa1
Tanya Shemet <tanyshak@gmail.com>
2023-05-12 12:18:10 +0200

    initial get_columns_in_relation

--- dbt/include/upsolver/macros/catalog.sql
+++ dbt/include/upsolver/macros/catalog.sql
@@ -1,7 +1,22 @@
 {{% macro upsolver__get_catalog(information_schema, schemas)-%}}
+  {{ log("information_schema " ~ information_schema ) }}
+  {{ log("schemas " ~ schemas ) }}
 
+  {%- if (schemas | length) == 0 -%}
    {{%set msg -%}}
-    get_catalog not implemented for upsolver
-   -%}} endset {{%
-  {{{{ exceptions.raise_compiler_error(msg) }}}}
- {{% endmacro %}}
+    There is no schemas
+   {{%endset-%}}
+  {%- else -%}
+  {%- set query -%}
+
+  {%- endset -%}
+
+  {%- endif -%}
+    select * from system.information_schema.columns
+    WHERE catalog = 'default_glue_catalog' AND
+    {%- for schema in schemas -%}
+    schema = '{{ schema }}'{%- if not loop.last %} or {% endif -%}
+
+  {{ return(run_query(query)) }}
+
+{{% endmacro %}}
