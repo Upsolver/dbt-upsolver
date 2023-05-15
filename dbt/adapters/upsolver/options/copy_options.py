a5482acea99549a54dd021d5abba9b3c55167ebf
Tanya Shemet <tanyshak@gmail.com>
2023-05-15 14:32:07 +0200

    fixed list options

--- dbt/adapters/upsolver/options/copy_options.py
+++ dbt/adapters/upsolver/options/copy_options.py
@@ -16,27 +16,27 @@ Copy_options = {
         "comment": {"type": "text", "editable": True, "optional": True}
     }
   },
-  "my_sql": {
+  "mysql": {
     "source_options": {
-        "table_include_list": {"type": "text", "editable": True, "optional": True},
-        "column_exclude_list": {"type": "text", "editable": True, "optional": True}
+        "table_include_list": {"type": "list", "editable": True, "optional": True},
+        "column_exclude_list": {"type": "list", "editable": True, "optional": True}
     },
     "job_options": {
-        "skip_snapshots": {"value": "text", "editable": True, "optional": True},
+        "skip_snapshots": {"type": "boolean", "editable": True, "optional": True},
         "end_at": {"type": "value", "editable": True, "optional": True},
         "compute_cluster": {"type": "identifier", "editable": True, "optional": True},
         "comment": {"type": "text", "editable": True, "optional": True}
     }
   },
-  "postgre_sql": {
+  "postgres": {
     "source_options": {
         "bucket": {"type": "text", "editable": False, "optional": False},
         "prefix": {"type": "text", "editable": False, "optional": True}
     },
     "job_options": {
-        "table_include_list": {"type": "text", "editable": False, "optional": True},
+        "table_include_list": {"type": "list", "editable": False, "optional": True},
         "skip_snapshots": {"type": "boolean", "editable": False, "optional": True},
-        "column_exclude_list": {"type": "text", "editable": False, "optional": True},
+        "column_exclude_list": {"type": "list", "editable": False, "optional": True},
         "publication_name": {"type": "text", "editable": False, "optional": False},
         "file_pattern": {"type": "text", "editable": False, "optional": True},
         "delete_files_after_load": {"type": "boolean", "editable": False, "optional": True},
@@ -60,5 +60,21 @@ Copy_options = {
         "compression": {"type": "value", "editable": False, "optional": True},
         "comment": {"type": "text", "editable": True, "optional": True}
     }
-  }
+  },
+    "kinesis": {
+      "source_options": {
+          "stream": {"type": "text", "editable": False, "optional": False}
+      },
+      "job_options": {
+          "reader_shards": {"type": "integer", "editable": True, "optional": True},
+          "store_raw_data": {"type": "boolean", "editable": False, "optional": True},
+          "start_from": {"type": "value", "editable": False, "optional": True},
+          "end_at": {"type": "value", "editable": False, "optional": True},
+          "compute_cluster": {"type": "identifier", "editable": True, "optional": True},
+          "run_parallelism": {"type": "integer", "editable": False, "optional": True},
+          "content_type": {"type": "value", "editable": True, "optional": True},
+          "compression": {"type": "value", "editable": False, "optional": True},
+          "comment": {"type": "text", "editable": True, "optional": True}
+      }
+    }
 }
